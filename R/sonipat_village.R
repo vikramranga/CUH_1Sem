82eac66a6b71c770e42c42cfd4587d8fd7d0433e
Vikram Ranga <vikram.ranga85@gmail.com>
2025-08-27 17:49:39 +0530

    Added a description for sonipat_village dataset

---
+++ R/sonipat_village.R
@@ -0,0 +1,18 @@
+#' Survey of India Sonipat village boundaries
+#'
+#' A Sample dataset for Master students at CUH Mahendragarh
+#'
+#'
+#' @format ## `sonipat_village`
+#' Simple feature collection with 324 features and 6 fields:
+#' \describe{
+#'   \item{n_v_code}{Village Code}
+#'   \item{n_v_name}{Name of the Village}
+#'   \item{n_t_code}{Tehsil Code}
+#'   \item{n_t_name}{Tehsil Name}
+#'   \item{n_d_code}{District Code}
+#'   \item{n_d_name}(District Name)
+#'   \item{geom}{Geometry}
+#' }
+#' @source Survey of India
+"sonipat_village"
